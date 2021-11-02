cebc4cb9ded603e69723457253412bbb145359fa
Memo- Aldu <71087733+Memo-Aldu@users.noreply.github.com>
2021-11-02 19:16:16 -0400

    SEG2505 Lab6 G10

---
+++ DataBaseProjectSql/app/src/main/java/com/example/databaseprojectsql/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.databaseprojectsql;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.databaseprojectsql.database.About;
+import com.example.databaseprojectsql.database.MyDBHandler;
+import com.example.databaseprojectsql.database.Product;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView idView;
+    EditText productBox;
+    EditText skuBox;
+    MyDBHandler dbHandler;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        idView = (TextView) findViewById(R.id.productID);
+        productBox = (EditText) findViewById(R.id.productName);
+        skuBox = (EditText) findViewById(R.id.productSku);
+        dbHandler = new MyDBHandler(this);
+    }
+
+
+
+    public void newProduct (View view) {
+
+        int sku = Integer.parseInt(skuBox.getText().toString());
+
+        Product product = new Product(productBox.getText().toString(), sku);
+
+        dbHandler.addProduct(product);
+
+        productBox.setText("");
+
+        skuBox.setText("");
+    }
+
+
+    public void lookupProduct (View view) {
+
+        Product product = dbHandler.findProduct(productBox.getText().toString());
+
+        if (product != null) {
+            idView.setText(String.valueOf(product.getID()));
+            skuBox.setText(String.valueOf(product.getSku()));
+        } else {
+            idView.setText("No Match Found");
+        }
+    }
+
+
+    public void removeProduct (View view) {
+
+
+        boolean result = dbHandler.deleteProduct(productBox.getText().toString());
+
+        if (result) {
+            idView.setText("Record Deleted");
+            productBox.setText("");
+            skuBox.setText("");
+        }
+        else
+            idView.setText("No Match Found");
+    }
+
+
+    public void about(View view) {
+        Intent aboutIntent = new Intent(this, About.class);
+        startActivity(aboutIntent);
+    }
+}
